bd631b3a0ae30ed730af10648befdc2eb7259820
Rogerio Van Rybroek Junior <rogeriorybroek@gmail.com>
2022-08-29 16:17:03 -0300

    adicionando o metodo delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -55,9 +55,21 @@ async function put(req, res) {
 
 }
 
+async function remove(req, res) {
+    const { id } = req.params
+
+    const remove = await ProductsModel.deleteOne({ _id: id })
+
+    const message = remove.acknowledged ? 'success' : 'error'
+
+    res.send({
+        message,
+    })
+}
+
 module.exports = {
     get,
     post,
     put,
-
+    remove,
 }
